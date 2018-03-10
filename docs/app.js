9c65279251526e6339b00ccae8673918b1fe3cf6
Matt Johnson <hello@mattpjohnson.com>
2018-03-10 14:56:05 -0500

    docs(demo): Don't use arrow functions

--- docs/app.js
+++ docs/app.js
@@ -13,33 +13,33 @@ require(['Darkflex'], function(Darkflex) {
     Darkflex.commandRegistry.registerCommandGroup(domGroup);
 
     const exampleGroup1 = new Darkflex.CommandGroup({ title: 'Example group 1', weight: 2 });
-    exampleGroup1.registerCommand({ title: 'Action 1', run: () => console.log('Group 1, Action 1') });
-    exampleGroup1.registerCommand({ title: 'Action 2', run: () => console.log('Group 1, Action 2') });
+    exampleGroup1.registerCommand({ title: 'Action 1', run: function() { console.log('Group 1, Action 1') }});
+    exampleGroup1.registerCommand({ title: 'Action 2', run: function() { console.log('Group 1, Action 2') }});
     Darkflex.commandRegistry.registerCommandGroup(exampleGroup1);
 
     const exampleGroup2 = new Darkflex.CommandGroup({ title: 'Example group 2' });
-    exampleGroup2.registerCommand({ title: 'Action 1', run: () => console.log('Group 2, Action 1') });
-    exampleGroup2.registerCommand({ title: 'Action 2', run: () => console.log('Group 2, Action 2') });
-    exampleGroup2.registerCommand({ title: 'Action 3', run: () => console.log('Group 2, Action 3') });
-    exampleGroup2.registerCommand({ title: 'Action 4', run: () => console.log('Group 2, Action 4') });
-    exampleGroup2.registerCommand({ title: 'Action 5', run: () => console.log('Group 2, Action 5') });
-    exampleGroup2.registerCommand({ title: 'Action 6', run: () => console.log('Group 2, Action 6') });
-    exampleGroup2.registerCommand({ title: 'Action 7', run: () => console.log('Group 2, Action 7') });
+    exampleGroup2.registerCommand({ title: 'Action 1', run: function() { console.log('Group 2, Action 1') }});
+    exampleGroup2.registerCommand({ title: 'Action 2', run: function() { console.log('Group 2, Action 2') }});
+    exampleGroup2.registerCommand({ title: 'Action 3', run: function() { console.log('Group 2, Action 3') }});
+    exampleGroup2.registerCommand({ title: 'Action 4', run: function() { console.log('Group 2, Action 4') }});
+    exampleGroup2.registerCommand({ title: 'Action 5', run: function() { console.log('Group 2, Action 5') }});
+    exampleGroup2.registerCommand({ title: 'Action 6', run: function() { console.log('Group 2, Action 6') }});
+    exampleGroup2.registerCommand({ title: 'Action 7', run: function() { console.log('Group 2, Action 7') }});
     Darkflex.commandRegistry.registerCommandGroup(exampleGroup2);
 
     // This group will show at the top since it has a higher weight than the others.
     const exampleGroup3 = new Darkflex.CommandGroup({ title: 'Example group 3', weight: 4 });
-    exampleGroup3.registerCommand({ title: 'Action 1', run: () => console.log('Group 3, Action 1') });
-    exampleGroup3.registerCommand({ title: 'Action 2', run: () => console.log('Group 3, Action 2') });
-    exampleGroup3.registerCommand({ title: 'Action 3', run: () => console.log('Group 3, Action 3') });
-    exampleGroup3.registerCommand({ title: 'Action 4', run: () => console.log('Group 3, Action 4') });
-    exampleGroup3.registerCommand({ title: 'Action 5', run: () => console.log('Group 3, Action 5') });
-    exampleGroup3.registerCommand({ title: 'Action 6', run: () => console.log('Group 3, Action 6') });
-    exampleGroup3.registerCommand({ title: 'Action 7', run: () => console.log('Group 3, Action 7') });
-    exampleGroup3.registerCommand({ title: 'Action 8', run: () => console.log('Group 3, Action 8') });
-    exampleGroup3.registerCommand({ title: 'Action 9', run: () => console.log('Group 3, Action 9') });
-    exampleGroup3.registerCommand({ title: 'Action 10', run: () => console.log('Group 3, Action 10') });
-    exampleGroup3.registerCommand({ title: 'Action 11', run: () => console.log('Group 3, Action 11') });
+    exampleGroup3.registerCommand({ title: 'Action 1', run: function() { console.log('Group 3, Action 1') }});
+    exampleGroup3.registerCommand({ title: 'Action 2', run: function() { console.log('Group 3, Action 2') }});
+    exampleGroup3.registerCommand({ title: 'Action 3', run: function() { console.log('Group 3, Action 3') }});
+    exampleGroup3.registerCommand({ title: 'Action 4', run: function() { console.log('Group 3, Action 4') }});
+    exampleGroup3.registerCommand({ title: 'Action 5', run: function() { console.log('Group 3, Action 5') }});
+    exampleGroup3.registerCommand({ title: 'Action 6', run: function() { console.log('Group 3, Action 6') }});
+    exampleGroup3.registerCommand({ title: 'Action 7', run: function() { console.log('Group 3, Action 7') }});
+    exampleGroup3.registerCommand({ title: 'Action 8', run: function() { console.log('Group 3, Action 8') }});
+    exampleGroup3.registerCommand({ title: 'Action 9', run: function() { console.log('Group 3, Action 9') }});
+    exampleGroup3.registerCommand({ title: 'Action 10', run: function() { console.log('Group 3, Action 10') }});
+    exampleGroup3.registerCommand({ title: 'Action 11', run: function() { console.log('Group 3, Action 11') }});
     Darkflex.commandRegistry.registerCommandGroup(exampleGroup3);
 
     Darkflex.commandRegistry.useDotEventListener();
