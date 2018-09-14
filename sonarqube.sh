39da7d2154bd9c96e32af4215e2d33fdbc18c0f6
Matt Johnson <matt@mattpjohnson.com>
2018-09-14 16:50:11 -0400

    chore(CI): Fix sonarqube.sh

--- sonarqube.sh
+++ sonarqube.sh
@@ -2,7 +2,7 @@ npx sonarqube-scanner \
   -Dsonar.projectKey=mattpjohnson_dotmenu \
   -Dsonar.organization=mattpjohnson-github \
   -Dsonar.sources=. \
-  -Dsonar.tests=.
+  -Dsonar.tests=. \
   -Dsonar.exclusions=node_modules/**,coverage/**,dist/** \
   -Dsonar.host.url=https://sonarcloud.io \
   -Dsonar.login=$SONAR_LOGIN
