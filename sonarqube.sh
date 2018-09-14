44cc24b23b6ef8bbd6a626184077e1930d9e6087
Matt Johnson <matt@mattpjohnson.com>
2018-09-14 17:07:46 -0400

    chore(CI): Add coverage to SonarQube

--- sonarqube.sh
+++ sonarqube.sh
@@ -5,5 +5,7 @@ npx sonarqube-scanner \
   -Dsonar.tests=. \
   -Dsonar.test.inclusions=**/*.test.js \
   -Dsonar.exclusions=node_modules/**,coverage/**,dist/**,**/*.test.js \
+  -Dsonar.javascript.lcov.reportPaths=coverage/jest/lcov.info \
+  -Dsonar.testExecutionReportPaths=coverage/jest/testlog.xml \
   -Dsonar.host.url=https://sonarcloud.io \
   -Dsonar.login=$SONAR_LOGIN
